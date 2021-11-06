dd4c37e06ba29b6ce8a3e70e04df568f80e291c9
David Glass <glassd@gmail.com>
2021-11-06 11:22:18 -0400

    prepare for github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import './Components/CardList/CardList.component'
+import { CardList } from './Components/CardList/CardList.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type='search' placeholder='Search Monsters' onChange={e => this.setstate({ searchField: e.target.value })} />
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/CardList/CardList.component.jsx
@@ -0,0 +1,12 @@
+import './CardList.style.css'
+import { Card } from '../Card/Card.component'
+
+export const CardList = props => {
+    return (
+        <div className='card-list'>
+            {props.monsters.map(monster => (
+                <Card key={monster.id} monster={monster} />
+            ))}
+        </div>
+    )
+}
